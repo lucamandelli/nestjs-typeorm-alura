45fa947674d5f13e8b56c84cbc701cf8dbe53d6f
lucamandelli <luca.mandelli@poatek.com>
2023-06-13 16:26:45 -0300

    created all configs for database and created first entity

---
+++ src/config/postgres.config.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmOptionsFactory } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm/dist";
+import { UsuarioEntity } from "src/usuario/usuario.entity";
+
+@Injectable()
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) { }
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: [],
+      synchronize: true
+    }
+  }
+}
